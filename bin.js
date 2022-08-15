90ff71292b73b678b2b9cbc10ffb72597d444fff
Kuss <kussssss@outlook.com>
2022-08-15 16:25:06 +0800

    feat: initial

---
+++ bin.js
@@ -0,0 +1,66 @@
+import cac from 'cac'
+import { openUrl } from './dist/index.mjs'
+import kleur from 'kleur'
+
+const cli = cac('incu')
+
+cli.command('url', 'Open url in Android/iOS simulator')
+
+cli.option('--only-android', 'Only open url in Android through ADB', {
+  default: false
+})
+cli.option('--only-ios', 'Only open url in iOS Simulator', {
+  default: false
+})
+
+cli.help()
+
+const parsed = cli.parse()
+
+const { args, h, onlyAndroid, onlyIos } = parsed
+
+const main = async () => {
+  if (h) {
+    return
+  }
+
+  const url = args[0]
+
+  const result = (platforms) => {
+    console.log(`Open ${kleur.blue().underline(url)} in iNCU\n`)
+    platforms.forEach(platform => {
+      const status = platform.ok ? '✅' : '❌'
+      console.log(`${status} ${platform.name}`)
+    })
+    console.log()
+  }
+
+  if (url) {
+    if (!onlyAndroid && !onlyIos) {
+      const androidOk = await openUrl(url, 'android')
+      const iosOk = await openUrl(url, 'ios', true)
+      result([
+        { name: 'Android', ok: androidOk },
+        { name: 'iOS Simulator', ok: iosOk }
+      ])
+      return
+    }
+    if (onlyAndroid && onlyIos) {
+      throw new Error('You should specify one of --only-android or --only-ios')
+    }
+    if (onlyAndroid) {
+      const ok = await openUrl(url, 'android')
+      result([
+        { name: 'Android', ok}
+      ])
+    }
+    if (onlyIos) {
+      const ok = await openUrl(url, 'ios')
+      result([
+        { name: 'iOS Simulator', ok }
+      ])
+    }
+  }
+}
+
+main()
---
+++ src/incu.ts
@@ -0,0 +1,36 @@
+import { openUrlSimulator } from './simulator'
+import { openUrlAdb } from './adb'
+
+export const iNCUScheme = (postfix: string) => `incu://${postfix}`
+
+export const iNCUWebViewScheme = (url: string) =>
+  iNCUScheme(`Webview?url=${url}`)
+
+const openUrls = {
+  android: openUrlAdb,
+  ios: openUrlSimulator,
+}
+
+export type Platform = 'android' | 'ios'
+
+/**
+ * open url in iNCU Webview
+ * @param url
+ * @param platform 'android' | 'ios'
+ * @returns boolean
+ */
+export const openUrl = async (url: string, platform: Platform, slient?: boolean) => {
+  try {
+    const openUrl = openUrls[platform]
+    if (!openUrl) {
+      throw new Error(`platform ${platform} not supported`)
+    }
+    await openUrl(iNCUWebViewScheme(url))
+    return true
+  } catch (e: any) {
+    if (!slient) {
+      console.error(e.stderr ?? e)
+    }
+    return false
+  }
+}
